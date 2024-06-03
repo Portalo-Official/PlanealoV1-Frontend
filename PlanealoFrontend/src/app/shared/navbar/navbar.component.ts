d78d3631e8fdf68e484e70e0a340763b16c16040
santiagoieshna <smiguezc01@iesarroyoharnina.es>
2024-06-03 13:37:11 +0200

    iconos navbar

--- PlanealoFrontend/src/app/shared/navbar/navbar.component.ts
+++ PlanealoFrontend/src/app/shared/navbar/navbar.component.ts
@@ -3,11 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styles: [ ` nav{  background-color: #f2a4fb !important;}` ]
-
-
-
+  styles: [ ` nav{  background-color: #f2a4fb !important;}`
+             , `nav-titulo{  font-size: .2rem;}`]
 })
 export class NavbarComponent {
+
+  userLogging = null;
+
+
 
 }
